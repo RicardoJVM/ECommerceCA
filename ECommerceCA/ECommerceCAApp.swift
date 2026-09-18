36011ef5e9eb0f7f502c3f796501b3de10c3e259
Ricardo Valencia <rvalencia@t2company.com.uy>
2026-09-18 11:08:39 -0300

    Initial Commit

---
+++ ECommerceCA/ECommerceCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceCAApp.swift
+//  ECommerceCA
+//
+//  Created by Ricardo Valencia on 18/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
